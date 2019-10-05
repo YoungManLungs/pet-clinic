98ca8923d83893494140ae656ec5daeaa8e4a46c
DoubleDurian <thedoubledurian@gmail.com>
2019-10-05 14:48:25 -0500

    Refactored Interfaces to remove duplicate code

--- pet-clinic-data/src/main/java/com/example/petclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/example/petclinic/model/Person.java
@@ -1,8 +1,9 @@
 package com.example.petclinic.model;
 
-public class Person {
+public class Person extends BaseEntity{
 
     private String firstName;
+
     private String lastName;
 
     public String getFirstName() {
