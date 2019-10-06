735196fe8d1985fff12d11ab29307738e907c35c
DoubleDurian <thedoubledurian@gmail.com>
2019-10-06 17:50:54 -0500

    adds visit entity and updated owner pet relationship

--- pet-clinic-data/src/main/java/com/example/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/example/petclinic/model/Owner.java
@@ -1,4 +1,14 @@
 package com.example.petclinic.model;
 
+import java.util.Set;
+
 public class Owner extends Person {
+
+    private Set<Pet> pets;
+
+    public Owner(Set<Pet> pets) {
+        this.pets = pets;
+    }
+
+
 }
